45f5b75dc6f701aba507c1ae80a42dbfeccaa63e
imooc <271708731@qq.com>
2020-01-29 18:58:22 +0800

    bio版本的rpc-demo-动态代理完成，待补充代理逻辑

--- src/main/java/com/lucheng/App.java
+++ src/main/java/com/lucheng/App.java
@@ -1,5 +1,6 @@
 package com.lucheng;
 
+import com.lucheng.mydubbo_1.impl.HelloServiceImpl;
 import com.lucheng.mydubbo_1.inter.HelloService;
 import com.lucheng.mydubbo_1.proxy.ProxyFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -13,8 +14,8 @@ public class App
     public static void main( String[] args )
     {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring/spring-config.xml");
-        HelloService  helloService = (HelloService) ProxyFactory.getInstance(HelloService.class);
-
-//        helloService.getName("lucheng");
+        HelloService helloService = ProxyFactory.getInstance(HelloService.class);
+        //结果是 InvocationHandler.invoke的返回值
+        helloService.getName("lucheng");
     }
 }
--- src/main/java/com/lucheng/mydubbo_1/proxy/ProxyFactory.java
+++ src/main/java/com/lucheng/mydubbo_1/proxy/ProxyFactory.java
@@ -8,11 +8,11 @@ import java.lang.reflect.Proxy;
  * 代理工厂
  */
 public class ProxyFactory {
-    private static InvocationHandler handler = new InvocationHandler() {
+    public static InvocationHandler handler = new InvocationHandler() {
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
             //在具体的代理方法内部实现rpc客户端的远程调用
-            System.out.print(proxy);
+            System.out.print("qwe");
             return null;
         }
     };
@@ -21,8 +21,11 @@ public class ProxyFactory {
      * 获取接口的代理类
      * @param clazz
      * @return
+     * 泛型方法
      */
-    public static Object getInstance(Class clazz){
-        return Proxy.newProxyInstance(clazz.getClassLoader(),clazz.getInterfaces(),handler);
+    public static <T> T getInstance(Class<T> clazz){
+        // clazz 若是接口则代理类需要的接口为：new Class[]{clazz}
+        //不是 clazz.getInterfaces() --> clazz 是接口的实现类时可以使用
+        return (T) Proxy.newProxyInstance(clazz.getClassLoader(),new Class[]{clazz},handler);
     }
 }
