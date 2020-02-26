4a392facaf48f674cb9926fb89290ee6d8120e65
lucheng28 <lucheng28@jd.com>
2020-02-26 15:04:34 +0800

    nio版本的rpc-demo与BIO的区别主要早nioClient里面，server的逻辑与client相同，nio属于同步非阻塞io，并不属于异步非阻塞io

---
+++ src/main/java/com/lucheng/mydubbo_2/App.java
@@ -0,0 +1,10 @@
+package com.lucheng.mydubbo_2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ){}
+}
---
+++ src/main/java/com/lucheng/mydubbo_2/inter/HelloService.java
@@ -0,0 +1,8 @@
+package com.lucheng.mydubbo_2.inter;
+
+/**
+ * 远程调用接口
+ */
+public interface HelloService {
+    String getName(String name);
+}
