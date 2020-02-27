b0a392d5286e30faf875a8e17b0d79f153c3e174
lucheng28 <lucheng28@jd.com>
2020-02-27 13:03:13 +0800

    netty的定长文本解析器来解决沾包问题

--- src/main/java/com/lucheng/client_1/netty_learning/solvezhanbao/delimeter/client/DelimeterSolveClient.java
+++ src/main/java/com/lucheng/client_1/netty_learning/solvezhanbao/delimeter/client/DelimeterSolveClient.java
@@ -11,10 +11,12 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
 import io.netty.handler.codec.string.StringDecoder;
 
 /**
  * netty的分隔符解析器解决沾包问题
+ *  FixedLengthFrameDecoder(20) 支持按照包的长度来解析
  */
 public class DelimeterSolveClient {
     public static void main(String[] args) {
