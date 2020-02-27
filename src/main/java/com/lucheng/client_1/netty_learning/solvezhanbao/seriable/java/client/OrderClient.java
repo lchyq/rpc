db4d49c9cb0974ddf179cf7e0a510325c79c1951
lucheng28 <lucheng28@jd.com>
2020-02-27 15:29:31 +0800

    基于java的netty序列化

---
+++ src/main/java/com/lucheng/client_1/netty_learning/solvezhanbao/seriable/java/client/OrderClient.java
@@ -0,0 +1,50 @@
+package com.lucheng.client_1.netty_learning.solvezhanbao.seriable.java.client;
+
+import com.lucheng.client_1.netty_learning.solvezhanbao.delimeter.client.DelimeterSolveHandler;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.serialization.ClassResolver;
+import io.netty.handler.codec.serialization.ClassResolvers;
+import io.netty.handler.codec.serialization.ObjectDecoder;
+import io.netty.handler.codec.serialization.ObjectEncoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * 基于java序列化的 netty序列化
+ */
+public class OrderClient {
+    public static void main(String[] args) {
+        EventLoopGroup work = new NioEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.group(work).
+                channel(NioSocketChannel.class)
+                .option(ChannelOption.TCP_NODELAY,true)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline() //下面三个handler是按顺序使用的
+                                //第一个参数：最多解析1024个对象，使用弱引用来缓存类加载器
+                                .addLast(new ObjectDecoder(1024,
+                                        ClassResolvers.weakCachingConcurrentResolver(this.getClass().getClassLoader())))
+                                .addLast(new ObjectEncoder())
+                                .addLast(new OrderClientHandler());
+                    }
+                });
+        try {
+            ChannelFuture future = bootstrap.connect("localhost",8080).sync();
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            work.shutdownGracefully();
+        }
+    }
+}
